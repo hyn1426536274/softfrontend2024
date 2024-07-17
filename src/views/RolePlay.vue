deb0075d6b8c5725c375bff27a3dc69dcb5f73ab
hyn1426536274 <1426536274@qq.com>
2024-07-18 01:50:20 +0800

    更新角色模拟和其他

--- src/views/RolePlay.vue
+++ src/views/RolePlay.vue
@@ -8,10 +8,10 @@
         <p>{{ roles[this.current_role] }}</p>
       </div>
       <div class="role_img">
-        <img id="roleimg" v-show="current_role===0" src="../../public/images/roles/lb.jpg" alt="Role Image">
-        <img id="roleimg" v-show="current_role===1" src="../../public/images/roles/zg.jpg" alt="Role Image">
-        <img id="roleimg" v-show="current_role===2" src="../../public/images/roles/gs.jpg" alt="Role Image">
-        <img id="roleimg" v-show="current_role===3" src="../../public/images/roles/jll.jpg" alt="Role Image">
+        <img id="roleimg" v-show="current_role === 0" src="../../public/images/roles/lb.jpg" alt="Role Image">
+        <img id="roleimg" v-show="current_role === 1" src="../../public/images/roles/zg.jpg" alt="Role Image">
+        <img id="roleimg" v-show="current_role === 2" src="../../public/images/roles/gs.jpg" alt="Role Image">
+        <img id="roleimg" v-show="current_role === 3" src="../../public/images/roles/jll.jpg" alt="Role Image">
       </div>
     </div>
 
@@ -33,7 +33,7 @@
 
       <div class="chat-container">
         <div class="chat-messages" ref="messagesContainer">
-          <div v-for="message in messages" :key="message"
+          <div v-for="message in messages[this.current_role]" :key="message"
             :class="message.type === 'user' ? 'user-message' : 'bot-message'">
             <p class="text" v-html="renderMarkdown(message.text)">
             </p>
@@ -65,7 +65,6 @@
 </template>
 <script>
 import axios from 'axios';
-axios.defaults.withCredentials = true;
 import SiriWave from "siriwave";
 import { parse } from 'marked';
 import LoadingAnimation from '@/components/LoadingAnimation.vue';
@@ -88,10 +87,18 @@ export default {
       is_shooted: false,
 
       messages: [
-        { type: 'user', text: 'Hello' },
-        { type: 'bot', text: '## Hi, how can I help you?' },
-        { type: 'user', text: 'I want to know about your products' },
-        { type: 'bot', text: '**Sure**, ---  what do you want to know? --- ' }
+        [
+          { type: 'bot', text: "你好，我是李白，有什么问题可以问我哟~" },
+        ],
+        [
+          { type: 'bot', text: "你好，我是诸葛亮，有什么问题可以问我哟~" },
+        ],
+        [
+          { type: 'bot', text: "你好，我是高斯，有什么问题可以问我哟~" },
+        ],
+        [
+          { type: 'bot', text: "你好，我是伽利略，有什么问题可以问我哟~" },
+        ],
       ],
       siriwave: null,
       isRecording: true,
@@ -106,11 +113,23 @@ export default {
       chatEnabled: true, // 是否继续聊天(用于终止聊天循环)
       roles: ['李白', '诸葛亮', '高斯', '伽利略'],
       current_role: 0, // 1:李白 2:诸葛亮 3:高斯 4:伽利略
-      role_img_path: [
-        '../../public/images/roles/lb.jpg', 
-        '../../public/images/roles/zg.jpg', 
-        '../../public/images/roles/gs.jpg', 
-        '../../public/images/roles/jll.jpg'
+      // role_img_path: [
+      //   '../../public/images/roles/lb.jpg', 
+      //   '../../public/images/roles/zg.jpg', 
+      //   '../../public/images/roles/gs.jpg', 
+      //   '../../public/images/roles/jll.jpg'
+      // ],
+      role_id: [
+        'b505bf4d019ebbe852f49b212e70a1ec', // 李白
+        '31bea398de488dccb6b8d94a22ee8fca', // 诸葛亮
+        'e0f55407b476d0a3a8521aaba3b9eeda', // 高斯
+        '24bc8c9e8db531aa55c67f466594a9f2' // 伽利略
+      ],
+      role_chatid: [ // 分别计数
+        1,
+        1,
+        1,
+        1,
       ]
     };
   },
@@ -139,12 +158,17 @@ export default {
   unmounted() {
     clearInterval(this.recordinterval);
     clearInterval(this.scrollinterval);
+
+    for (let i = 0; i < this.role_id.length; i++) {
+      this.clearChatRecord(this.role_id[i]);
+    }
   },
   methods: {
+
     changeRole(role) {
       this.current_role = role;
     },
-  
+
     renderMarkdown(text) {
       return parse(text);
     },
@@ -197,23 +221,23 @@ export default {
       try {
         // 每次只能执行一个步骤
         console.log('chat');
-        if(this.ChatStep === 1){
+        if (this.ChatStep === 1) {
           await this.startRecording();
         }
-        if(this.ChatStep === 2){
+        if (this.ChatStep === 2) {
           await this.audioToText();
         }
-        if(this.ChatStep === 3){
+        if (this.ChatStep === 3) {
           await this.sparkChat();
         }
-        
+
       } catch (error) {
         console.error(error);
       }
     },
-    async chatLoop(){
-      try{
-        if(this.chatEnabled){
+    async chatLoop() {
+      try {
+        if (this.chatEnabled) {
           await this.chat();
           this.chatLoop();
         }
@@ -223,14 +247,14 @@ export default {
       }
     },
     stopChat() {
-    this.chatEnabled = false;
-  },
+      this.chatEnabled = false;
+    },
     scrollToBottom() {
       const container = this.$refs.messagesContainer;
       container.scrollTop = container.scrollHeight;
     },
     async startRecording() {
-      try{
+      try {
         console.log('Record Start', this.ChatStep, this.chatEnabled);
         const response = await axios.get('http://127.0.0.1:8000/录音');
         console.log(response.data.result);
@@ -259,40 +283,58 @@ export default {
 
         console.log('response', response);
         this.current_question = this.ResponseFilter(response.data.result);
-        if(this.current_question === '0'){
-          this.messages.push({ type: 'bot', text: "对不起，没有听清，再说一遍哟~" });
+        if (this.current_question === '0') {
+          this.messages[this.current_role].push({ type: 'bot', text: "对不起，没有听清，再说一遍哟~" });
           this.ChatStep = 1; // 重新进入语音录音状态
           return;
         }
 
-        this.messages.push({ type: 'user', text: this.current_question });
+        this.messages[this.current_role].push({ type: 'user', text: this.current_question });
 
         this.ChatStep = 3; // Spark对话状态
       } catch (error) {
         this.stopChat();
         console.error(error);
-        
+
       }
     },
 
     async sparkChat() {
       try {
-
         const formData = new FormData();
-        formData.append('message', this.current_question);
-        formData.append('image_path', this.imagepath);
-        const response = await axios.post('http://127.0.0.1:8000/对象识别', formData,
+        formData.append('msg', this.current_question);
+        if (this.role_chatid[this.current_role] <= 1) {
+          // 第一次问答 chatid = prechatid
+          formData.append('chatid', this.role_chatid[this.current_role]);
+          formData.append('prechatid', this.role_chatid[this.current_role]);
+          this.role_chatid[this.current_role] = this.role_chatid[this.current_role] + 1;
+        }
+        else {
+          formData.append('chatid', this.role_chatid[this.current_role]);
+          formData.append('prechatid', this.role_chatid[this.current_role]-1);
+          this.role_chatid[this.current_role] = this.role_chatid[this.current_role] + 1;
+        }
+
+        formData.append('agentid', this.role_id[this.current_role]);
+        console.log('formData:', this.current_question, this.role_chatid[this.current_role] + 1, this.role_chatid[this.current_role], this.role_id[this.current_role]);
+        const response = await axios.post('http://127.0.0.1:5000/talk', formData,
           {
             headers: {
               // 'Content-Type': 'multipart/form-data'
               'Content-Type': 'application/json',
             },
-            withCredentials: true
           });
+        
 
         console.log(response.data);
-        this.current_answer = response.data.result;
-        this.messages.push({ type: 'bot', text: this.current_answer });
+        this.current_answer = response.data.message;
+        if (!response.data.message) {
+          this.messages[this.current_role].push({ type: 'bot', text: "对不起，我不知道怎么回答你的问题" });
+          this.ChatStep = 1; // 重新进入语音录音状态
+          return;
+        }
+        console.log('current_answer:', this.current_answer);
+        this.messages[this.current_role].push({ type: 'bot', text: this.current_answer });
 
         this.ChatStep = 1; // 重新进入语音录音状态
       } catch (error) {
@@ -300,7 +342,25 @@ export default {
         console.error(error);
       }
     },
+    async clearChatRecord(roleid) { // 结束对话，清除聊天缓存
+      try {
+        const formData = new FormData();
+        formData.append('agentid', roleid);
+        formData.append('chatid', this.role_chatid[this.current_role]);
+        const response = await axios.post('http://127.0.0.1:5000/end', formData,
+          {
+            headers: {
+              // 'Content-Type': 'multipart/form-data'
+              'Content-Type': 'application/json',
+            },
+          });
+        console.log(response.data);
 
+      } catch (error) {
+        this.stopChat();
+        console.error(error);
+      }
+    },
 
   }// methods
 }
@@ -340,10 +400,12 @@ export default {
   background-color: #e1e6bb9b;
   margin: 0 10px;
 }
-.role_name{
+
+.role_name {
   display: flex;
   justify-content: center;
   align-items: center;
+
   p {
     font-size: 30px;
     font-weight: bold;
@@ -351,12 +413,14 @@ export default {
   }
 
 }
+
 .role_img {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 80%;
   padding-top: 20px;
+
   img {
     width: 80%;
     height: 100%;
@@ -391,19 +455,21 @@ export default {
     margin: 2px auto;
     background-color: #efefefad;
     border-radius: 5px;
-    color:rgb(54, 35, 0);
+    color: rgb(54, 35, 0);
   }
 }
+
 .btn_contain {
   display: flex;
   justify-content: space-between;
   width: 300px;
   border-radius: 8px;
   flex-wrap: wrap;
+
   .change_role {
     width: 130px;
     height: 50px;
-    margin: 8px 10px; 
+    margin: 8px 10px;
     text-align: center;
     line-height: 50px;
     border-radius: 8px;
@@ -412,9 +478,10 @@ export default {
     transition: all 0.3s ease;
     font-size: 20px;
     font-weight: bold;
-    color:#6d4100;
+    color: #6d4100;
 
   }
+
   .change_role:hover {
     background-color: #ff9900;
   }
@@ -484,12 +551,14 @@ export default {
   }
 
 }
-.loading_contain{
+
+.loading_contain {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 160px;
   width: 100%;
 }
+
 /* 聊天部分: 结束 */
 </style>
